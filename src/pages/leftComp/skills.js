d94f21357ec0e255f87b4a0d1b728db9cc01fb20
Vishnu <vishnupanickers1@gmail.com>
2022-03-13 20:34:06 +0530

    name changed to skills.js

---
+++ src/pages/leftComp/skills.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+
+
+function Skills() {
+  return (
+    <>
+      <div className="skill flex grid-container">
+        <h5 className="grid-item">HTML</h5>
+        <h5 className="grid-item">CSS</h5>
+        <h5 className="grid-item">JavaScript</h5>
+        <h5 className="grid-item">c++</h5>
+        <h5 className="grid-item">Python</h5>
+        <h5 className="grid-item">Django</h5>
+        <h5 className="grid-item">NumPy</h5>
+        <h5 className="grid-item">Pandas</h5>
+        <h5 className="grid-item">Seaborn</h5>
+        <h5 className="grid-item">MatplotLib</h5>
+        <h5 className="grid-item">Scikit-Learn</h5>
+      </div>
+    </>
+  );
+}
+
+export default Skills;
